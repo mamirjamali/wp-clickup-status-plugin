bbcf039ed4871e67c7d9cd221c38da14bf1db57c
Amir Jamali <62475538+mamirjamali@users.noreply.github.com>
2024-03-09 12:38:25 +0400

    Add a function to check if bootstrap has already been loaded

--- src/ClickUpStatusForm.php
+++ src/ClickUpStatusForm.php
@@ -2,32 +2,44 @@
 namespace CUSTATUS\ClickUpStatusPlugin;
 
 class ClickUpStatusForm {
+
+    private static $bootstrap_classes_found;
+
     public static function clickup_status_form_shortcode() {
-        ob_start(); // Start output buffering
+
+        // Check if Bootstrap classes are already found
+        if (self::$bootstrap_classes_found === null) {
+            self::$bootstrap_classes_found = self::check_bootstrap_classes();
+        }
+
+        // Start output buffering
+        ob_start();
         ?>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.17.0/font/bootstrap-icons.css">
+        <?php if (!self::$bootstrap_classes_found) : ?>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.17.0/font/bootstrap-icons.css">
+            <style>
+               /* Add custom CSS for button alignment */
+                #clickup-status-checker .col-auto {
+                    display: flex;
+                    align-items: baseline;
+                }
+
+                #clickup-status-checker button {
+                    margin-left: 8px; /* Adjust the margin as needed */
+                }
+            </style>
+        <?php endif; ?>
+
         <link rel="stylesheet" href="<?php echo plugins_url('/assets/css/clickupStatus.css', __FILE__); ?>">
         <script type="text/javascript">
             var ajax_object = <?php echo json_encode(array('ajax_url' => admin_url('admin-ajax.php'))); ?>;
         </script>
         <script type="text/javascript" src="<?php echo plugins_url('/assets/js/clickupStatus.js', __FILE__); ?>"></script>
 
-        <style>
-            /* Add custom CSS for button alignment */
-            #clickup-status-checker .col-auto {
-                display: flex;
-                align-items: baseline;
-            }
-
-            #clickup-status-checker button {
-                margin-left: 8px; /* Adjust the margin as needed */
-            }
-        </style>
-
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12">
+                <div class="col">
                     <form id="clickup-status-checker" method="post" action="">
                         <div class="form-row align-items-center">
                              <input type="hidden" name="form_identifier" value="clickup_client_status">
@@ -53,4 +65,53 @@ class ClickUpStatusForm {
         <?php
         return ob_get_clean(); // Return the buffered content
     }
+
+    // Function to check if Bootstrap classes are present
+    private static function check_bootstrap_classes() {
+        // Retrieve the value from the option
+        $option_value = get_option('csp_bootstrap_classes_not_found', null);
+
+        // If the option is set, return its value
+        if ($option_value !== null) {
+            return $option_value;
+        }
+
+        $stylesheets = wp_styles();
+        $bootstrap_classes = array(
+            'container',
+            'row',
+            'col-',
+        );
+
+        // Initialize a flag to check if Bootstrap classes are found
+        $bootstrap_found = false;
+
+        foreach ($stylesheets->registered as $stylesheet) {
+            // Check if the stylesheet has a valid source
+            if (!empty($stylesheet->src)) {
+                $file_content = @file_get_contents($stylesheet->src);
+
+                if ($file_content !== false) {
+                    foreach ($bootstrap_classes as $class) {
+                        if (strpos($file_content, $class) !== false) {
+                            // Set the flag and break out of the inner loop
+                            $bootstrap_found = true;
+                            break;
+                        }
+                    }
+
+                    // Break out of the outer loop if Bootstrap classes are found
+                    if ($bootstrap_found) {
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Update the option based on whether Bootstrap classes are found
+        update_option('csp_bootstrap_classes_not_found', !$bootstrap_found);
+
+        return !$bootstrap_found;
+    }
+
 }
